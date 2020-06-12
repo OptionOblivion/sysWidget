6934d0c7183d4bd2acfabc50fbfac864751dcf3c
Dean Hartley <64603053+OptionOblivion@users.noreply.github.com>
2020-06-11 20:54:58 -0400

    Update batteryGUI.py

--- batteryGUI.py
+++ batteryGUI.py
@@ -6,4 +6,8 @@ gui.geometry("300x400")
 gui.resizable(0,0)
 gui.configure(bg="royalblue")
 
+label_life = Label(gui,  text='Battery Life: ',fg="black", font="none 16 bold").grid(row=1,column=0)
+label_percentage= Label(gui, text='insert data here', fg="red", font="none 16 bold").grid(row=1,column=1)
+
+
 gui.mainloop()
