2f3e28d621f8a2733916112def873ae03755d7a3
Dean Hartley <64603053+OptionOblivion@users.noreply.github.com>
2020-06-12 09:08:15 -0400

    Update batteryGUI.py

--- batteryGUI.py
+++ batteryGUI.py
@@ -1,16 +1,32 @@
 from tkinter import *
 
-gui = Tk()
-gui.title("PowerBoi 9000")
-gui.geometry("300x400")
-gui.resizable(0,0)
-gui.configure(bg="white")
+class GUI:
 
-label_life = Label(gui, text='Percentage:',fg="black", bg="white",font="none 16 bold underline").grid(row=0,column=0,  sticky=W)
-label_percentage= Label(gui, text='N/A', fg="red", font="none 16 bold").grid(row=0,column=1, sticky=W)
-label_time = Label(gui, text='Time Remaining:',fg="black", bg="white",font="none 16 bold underline").grid(row=1,column=0)
+    def __init__(self):
+        pass
 
-label_hours = Label(gui, text='N/A', fg="red", font="none 16 bold").grid(row=1,column=1)
+    gui = Tk()
+    gui.title("PowerBoi 9000")
+    gui.geometry("300x400")
+    gui.resizable(0,0)
+    label_life = Label(gui, text='Percentage:', fg="black", bg="white", font="none 16 bold underline").grid(row=0,column=0,sticky=W)
+    label_percentage = Label(gui, text='N/A', fg="red", font="none 16 bold").grid(row=0, column=1, sticky=W)
+    label_time = Label(gui, text='Time Remaining:', fg="black", bg="white", font="none 16 bold underline").grid(row=1,column=0)
+    label_hours = Label(gui, text='N/A', fg="red", font="none 16 bold").grid(row=1, column=1)
+    colorChange = Button(gui, text='Dark', activebackground="royalblue" ).grid(row=3, column=0)
+    colorChange = Button(gui, text='Light', activebackground="royalblue").grid(row=3, column=1)
+    gui.mainloop()
 
+class Battery:
 
-gui.mainloop()
+    def __init__(self):
+        percentage=0;
+        time=0;
+
+    def get_percentage(self):
+        pass
+        #will return percentage data
+
+    def get_time(self):
+        pass
+        #will return time remaining data
