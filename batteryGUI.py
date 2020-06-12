73a77cd05efe39ffa275d8bacb68554e31886ae0
Dean Hartley <64603053+OptionOblivion@users.noreply.github.com>
2020-06-12 09:11:56 -0400

    Update batteryGUI.py

--- batteryGUI.py
+++ batteryGUI.py
@@ -9,6 +9,9 @@ class GUI:
     gui.title("PowerBoi 9000")
     gui.geometry("300x400")
     gui.resizable(0,0)
+    icon = PhotoImage(file="icon.png")
+    gui.iconphoto(False, icon)
+
     label_life = Label(gui, text='Percentage:', fg="black", bg="white", font="none 16 bold underline").grid(row=0,column=0,sticky=W)
     label_percentage = Label(gui, text='N/A', fg="red", font="none 16 bold").grid(row=0, column=1, sticky=W)
     label_time = Label(gui, text='Time Remaining:', fg="black", bg="white", font="none 16 bold underline").grid(row=1,column=0)
