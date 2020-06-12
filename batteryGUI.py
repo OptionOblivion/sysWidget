6faa758d01c2aba6c5c2950e23330ef358e6e722
Dean Hartley <64603053+OptionOblivion@users.noreply.github.com>
2020-06-11 21:20:09 -0400

    Update batteryGUI.py

--- batteryGUI.py
+++ batteryGUI.py
@@ -4,10 +4,13 @@ gui = Tk()
 gui.title("PowerBoi 9000")
 gui.geometry("300x400")
 gui.resizable(0,0)
-gui.configure(bg="royalblue")
+gui.configure(bg="white")
 
-label_life = Label(gui,  text='Battery Life: ',fg="black", font="none 16 bold").grid(row=1,column=0)
-label_percentage= Label(gui, text='insert data here', fg="red", font="none 16 bold").grid(row=1,column=1)
+label_life = Label(gui, text='Percentage:',fg="black", bg="white",font="none 16 bold").grid(row=0,column=0,  sticky=W)
+label_percentage= Label(gui, text='N/A', fg="red", font="none 16 bold").grid(row=0,column=1, sticky=W)
+label_time = Label(gui, text='Time Remaining:',fg="black", bg="white",font="none 16 bold").grid(row=1,column=0)
+
+label_hours = Label(gui, text='N/A', fg="red", font="none 16 bold").grid(row=1,column=1)
 
 
 gui.mainloop()
