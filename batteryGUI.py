7277a276311f04201912b9554402d94641b1e993
Dean Hartley <64603053+OptionOblivion@users.noreply.github.com>
2020-06-12 12:25:33 -0400

    Added light and dark modes

--- batteryGUI.py
+++ batteryGUI.py
@@ -6,22 +6,24 @@ gui.geometry("300x400")
 gui.resizable(0, 0)
 icon = PhotoImage(file="icon.png")
 gui.iconphoto(False, icon)
-
-label_life = Label(gui, text='Percentage:', fg="black", bg="white", font="none 16 bold underline").grid(row=0,column=0, sticky=W)
+label_life = Label(gui, text='Percentage:', fg="black", font="none 16 bold underline").grid(row=0,column=0, sticky=W)
 label_percentage = Label(gui, text='N/A', fg="red", font="none 16 bold").grid(row=0, column=1, sticky=E)
 label_time = Label(gui, text='Time Remaining:', fg="black", bg="white", font="none 16 bold underline").grid(row=1,column=0,sticky=W)
 label_hours = Label(gui, text='N/A', fg="red", font="none 16 bold").grid(row=1, column=1, sticky=E)
 label_filler = Label(gui, bg='white').grid(row=2, column=0, pady=140)
 
-def filler():
+def darkMode():
     filler=Label(gui, bg='dimgrey').grid(row=2, column=0, pady=140)
+    percent_dark=Label(gui, text='Percentage:', fg='white', bg='dimgrey',font="none 16 bold underline").grid(row=0,column=0, sticky=W)
+    time_dark = Label(gui, text='Time Remaining:', fg="white", bg="dimgrey", font="none 16 bold underline").grid(row=1,column=0,sticky=W)
 
 def filler_wht():
     fillWhite=Label(gui, bg='white').grid(row=2, column=0, pady=140)
+    percent_light = Label(gui, text='Percentage:', fg='black', bg='white', font="none 16 bold underline").grid(row=0,column=0,sticky=W)
+    time_light = Label(gui, text='Time Remaining:', fg="black", bg="white", font="none 16 bold underline").grid(row=1,column=0,sticky=W)
 
-but_dark = Button(gui, text='Dark', command=lambda:[gui.configure(bg='dimgrey'),filler()]).grid(row=3,column=0)
+but_dark = Button(gui, text='Dark', command=lambda:[gui.configure(bg='dimgrey'),darkMode()]).grid(row=3,column=0)
 but_light = Button(gui, text='Light', command=lambda:[gui.configure(bg='white'),filler_wht()]).grid(row=3,column=1)
-
 gui.mainloop()
 
 class Battery:
