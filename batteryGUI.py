a9642512b38ee68319546cf43d71626362885799
Dean Hartley <64603053+OptionOblivion@users.noreply.github.com>
2020-06-13 13:25:52 -0400

    Update batteryGUI.py

--- batteryGUI.py
+++ batteryGUI.py
@@ -2,6 +2,8 @@ from tkinter import *
 import threading
 from sysinfo import *
 
+from SysInfo import *
+
 
 class SysinfoThread():
 
@@ -15,7 +17,7 @@ class SysinfoThread():
             pass
 
 
-class BatteryGUI():
+class BatteryGUI(CPU,RAM):
     """Class that contains the GUI. Possible problem is the extra thread will make this class be a global var, which breaks OOP """
 
     def __init__(self):
@@ -23,7 +25,7 @@ class BatteryGUI():
         self.gui.configure(bg='white')
         self.gui.title("Mac > PC")
         self.gui.geometry("230x400")
-        self.gui.resizable(0, 0)
+        #self.gui.resizable(0, 0)
         self.icon = PhotoImage(file="icon.png")
         self.gui.iconphoto(False, self.icon)
 
@@ -31,21 +33,24 @@ class BatteryGUI():
         self.label_life = Label(self.gui, text='Percentage:', fg="black",font="none 16 bold underline").grid(row=0,column=0,pady=15, sticky=W)
         self.label_percentage = Label(self.gui, text='N/A', fg="red", font="none 16 bold").grid(row=0, column=0,sticky=E)
         #Time Remaining Widget
-        self.label_time = Label(self.gui, text='Time Remaining: ', fg="black", bg="white",font="none 16 bold underline").grid(row=1, column=0, pady=15,sticky=W)
+        self.label_time = Label(self.gui, text='Time Remaining: ', fg="black",font="none 16 bold underline").grid(row=1, column=0, pady=15,sticky=W)
         self.label_hours = Label(self.gui, text='N/A', fg="red", font="none 16 bold").grid(row=1, column=0, sticky=E)
         #CPU Widget
+        x=CPU.get_cpu(self)
         self.label_cpu=Label(self.gui, text='CPU: ', fg='black', font="none 16 bold underline").grid(row=2, column=0, pady=15, sticky=W)
-        self.label_cpu_data=Label(self.gui, text='N/A', fg='red', font="none 16 bold").grid(row=2, column=0, sticky=E)
+        self.label_cpu_data=Label(self.gui, text=x, fg='red', font="none 16 bold").grid(row=2, column=0, sticky=E)
         #RAM Widget
+        ram=RAM.get_RAM(self)
         self.label_ram=Label(self.gui, text='RAM: ', fg='black', font='non 16 bold underline').grid(row=3, column=0, pady=15, sticky=W)
-        self.label_ram_data=Label(self.gui, text='N/A', fg='red', font='none 16 bold').grid(row=3, column=0, sticky=E)
+        self.label_ram_data=Label(self.gui, text=ram, fg='red', font='none 16 bold').grid(row=3, column=0, sticky=E)
         #GPU Widget
         self.label_gpu = Label(self.gui, text='GPU: ', fg='black', font='non 16 bold underline').grid(row=4, column=0,pady=15, sticky=W)
         self.label_gpu_data = Label(self.gui, text='N/A', fg='red', font='none 16 bold').grid(row=4, column=0, sticky=E)
         #spacer
         self.label_filler = Label(self.gui, bg='white').grid(row=5, column=0, pady=10)
-        #Refresh button
+        #Refresh button- need to program command to refresh data
         self.refresh = Button(self.gui, text='Refresh').grid(row=6,column=0, sticky=SE)
+        #Footer
         footer = PhotoImage(file="footer.gif")
         label_footer = Label(self.gui, image=footer)
         label_footer.image=footer
@@ -79,16 +84,3 @@ if __name__ == '__main__':
     GU.run()
 
 
-class Battery:
-
-    def __init__(self):
-        percentage = 0;
-        time = 0;
-
-    def get_percentage(self):
-        pass
-        # will return percentage data
-
-    def get_time(self):
-        pass
-        # will return time remaining data
